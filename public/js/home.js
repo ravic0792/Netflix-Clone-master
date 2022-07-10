a029c2ba8053edb8a2311df5b8b74227d3dfdf7e
ravic0792 <ravi.pratap_cs19@gla.ac.in>
2022-07-10 09:23:53 +0530

    first commit

---
+++ public/js/home.js
@@ -0,0 +1,71 @@
+const main = document.querySelector('.main');
+
+fetch(genres_list_http + new URLSearchParams({
+    api_key: api_key
+}))
+.then(res => res.json())
+.then(data => {
+    data.genres.forEach(item => {
+        fetchMoviesListByGenres(item.id, item.name);
+    })
+});
+const fetchMoviesListByGenres = (id, genres) => {
+    fetch(movie_genres_http + new URLSearchParams({
+        api_key: api_key,
+        with_genres: id,
+        page: Math.floor(Math.random() * 3) + 1
+    }))
+    .then(res => res.json())
+    .then(data => {
+        makeCategoryElement(`${genres}_movies`, data.results);
+    })
+    .catch(err =>  console.log(err));
+}
+const makeCategoryElement=(category,data)=>
+{
+        main.innerHTML+=`
+
+        <div class="movie-list">
+
+        <button class="pre-btn"><i class='fas fa-angle-left' style='font-size:30px;color:red'></i></button>
+    
+        <h1 class="movie-category">${category.split("_").join(" ")}</h1>
+    
+        <div class="movie-container" id="${category}">
+            
+               
+       </div>
+        
+    <button class="nxt-btn"><i class='fas fa-angle-right' style='font-size:36px;color:red'></i></button>
+    
+    </div>
+
+        
+     `;
+
+    makeCards(category, data);        
+}
+const makeCards = (id, data) => {
+    const movieContainer = document.getElementById(id);
+    data.forEach((item, i) => {
+        if(item.backdrop_path == null){
+            item.backdrop_path = item.poster_path;
+            if(item.backdrop_path == null){
+               return;
+           }
+         }
+         movieContainer.innerHTML += `
+<div class="movie" onclick="location.href = '/${item.id}'">
+    <img src="${img_url}${item.backdrop_path}" alt="">
+    <p class="movie-title">${item.title}</p>
+</div>
+
+`;
+if(i == data.length - 1){
+    setTimeout(() => {
+        setupScrolling();
+    }, 100);
+}
+
+    })
+}
